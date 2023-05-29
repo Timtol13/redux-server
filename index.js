1cf3575ebfefc979c347edcc8c622907a39c2b71
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-05-29 15:12:33 +0300

    fix messages and add posts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ws = require('ws')
 var fs = require('fs')
+const path = require('path')
 const multer  = require('multer')
 const sqlite3 = require('sqlite3'),  TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
 const cors = require('cors')
@@ -189,16 +190,89 @@ app.get('/getMessages/:userFrom/:userTo', (req, res) => {
   })
 })
 
+app.use('/images/posts', express.static(path.join(__dirname, 'images/posts')));
+
 app.get('/getPosts', (req, res) => {
-  db.all('SELECT * FROM posts', (err, rows) => {
-    if(err){
-      console.error("Error: " + err)
-    }else if (!rows) {
-      res.status(404).send('Messages not found');
+  db.all('SELECT * FROM posts', (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    } else if (!row) {
+      res.status(404).send('Photo not found');
     } else {
-      return res.json(rows)
+      res.status(200).json(row)
     }
   } )
+})
+app.post('/createPost', upload.single('photo'), (req, res) => {
+  const { login, name, surname, description } = req.body;
+  const photo = req.file;
+  if (photo){
+    const photoPath = path.join(__dirname, 'images/posts', photo.filename);
+    fs.renameSync(photo.path, photoPath);
+  }
+
+  const query = 'INSERT INTO posts (login, name, surname, description, photo) VALUES (?, ?, ?, ?, ?)';
+  db.run(query, [login, name, surname, description, photo? photo.filename: ''], function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка при сохранении записи в базу данных');
+    } else {
+      res.send('Запись успешно сохранена в базе данных');
+    }
+  });
+})
+
+app.get('/getPosts/:login', (req, res) => {
+  const {login} = req.params
+  db.all(`SELECT * FROM posts WHERE login = '${login}'`, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка при сохранении записи в базу данных');
+    } else {
+      res.json(rows);
+    }
+  })
+})
+
+app.put('/like', (req, res) => {
+  const {id} = req.body
+  db.get(`SELECT * FROM posts WHERE ID = ${id}`, (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка при сохранении записи в базу данных');
+    } else {
+      console.log(row)
+      db.run(`UPDATE posts SET like = '${Number.parseInt(row.like) + 1}'  WHERE ID = '${id}'`, (err, rows) => {
+        if (err) {
+          console.error(err)
+          res.status(500).send('Ошибка при сохранении записи в базу данных');
+        } else {
+          res.json(rows);
+        }
+      })
+    }
+  })
+})
+
+app.put('/unlike', (req, res) => {
+  const {id} = req.body
+  db.get(`SELECT * FROM posts WHERE ID = ${id}`, (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка при сохранении записи в базу данных');
+    } else {
+      console.log(row)
+      db.run(`UPDATE posts SET like = '${Number.parseInt(row.like) - 1}'  WHERE ID = '${id}'`, (err, rows) => {
+        if (err) {
+          console.error(err)
+          res.status(500).send('Ошибка при сохранении записи в базу данных');
+        } else {
+          res.json(rows);
+        }
+      })
+    }
+  })
 })
 
 
